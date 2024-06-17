ee0a31261459867f002171f7ab84651ec5379a76
lumamontes <lumagoesmontes@gmail.com>
2024-06-17 08:30:43 -0300

    add classes examples

--- classes/index.js
+++ classes/index.js
@@ -1,112 +1,40 @@
-class DatabaseError {
-    constructor(statement, message) {
-        this.statement = statement;
-        this.message = message;
-    }
-}
-class Parser {
-    constructor() {
-        this.commands = new Map();
-        this.commands.set("createTable", /create table ([a-z]+) \((.+)\)/);
-        this.commands.set("insert", /insert into ([a-z]+) \((.+)\) values \((.+)\)/);
-        this.commands.set("select", /select (.+) from ([a-z]+)(?: where (.+))?/);
-        this.commands.set("delete", /delete from ([a-z]+)(?: where (.+))?/);
-    }
-
-    parse(statement) {
-        for (let [command, regexp] of this.commands) {
-            const parsedStatement = statement.match(regexp);
-            if (parsedStatement) {
-                return {
-                    command,
-                    parsedStatement
+function createArray(){
+    return new Proxy({}, {
+        //Existem métodos, chamados de trap, para diversos tipos de eventos relacionados a um objetico como:
+        //apply, construct, defineProperty, deleteProperty, get, getOwnPropertyDescriptor, getPrototypeOf, has, isExtensible, ownKeys, preventExtensions, set, setPrototypeOf
+        set(target, key, value){
+            target.length = target.length || 0;
+            target.length++;
+            // target[key] = value;
+            Reflect.set(target, key, value);
+        },
+        get(target, key){
+            if(typeof key === "string" && key.match(/\d+/)){
+                if(!(key in target)){
+                    throw `Property ${key} not found`;
                 }
             }
+            return target[key]
+        },
+        deleteProperty(target, key){
+            if(key in target){
+                target.length--;
+                delete target[key];
+            }
         }
-    }
+    });
 }
-class Database {
-    constructor() {
-        this.tables = {};
-        this.parser = new Parser();
-    }
 
-    createTable(parsedStatement) {
-        let [,tableName, columns] = parsedStatement;
-        this.tables[tableName] = {
-            columns: {},
-            data: []
-        };
-        columns = columns.split(",");
-        for (let column of columns) {
-            column = column.trim().split(" ");
-            const [name, type] = column;
-            this.tables[tableName].columns[name] = type;
-        }
-    }
 
-    insert(parsedStatement) {
-        let [,tableName, columns, values] = parsedStatement;
-        columns = columns.split(", ");
-        values = values.split(", ");
-        let row = {};
-        for (let i = 0; i < columns.length; i++) {
-            const column = columns[i];
-            const value = values[i];
-            row[column] = value;
-        }
-        this.tables[tableName].data.push(row);
-    }
 
-    select(parsedStatement) {
-        let [, columns, tableName, whereClause] = parsedStatement;
-        columns = columns.split(", ");
-        let rows = this.tables[tableName].data
-        if (whereClause) {
-            let [columnWhere, valueWhere] = whereClause.split(" = ");
-            rows = rows.filter(function (row) {
-                return row[columnWhere] === valueWhere;
-            });
-        }
-        rows = rows.map(function (row) {
-            const selectedRow = {};
-            columns.forEach(function (column) {
-                selectedRow[column] = row[column];
-            });
-            return selectedRow;
-        });
-        return rows;
-    }
 
-    delete(parsedStatement) {
-        let [, tableName, whereClause] = parsedStatement;
-        if (whereClause) {
-            let [columnWhere, valueWhere] = whereClause.split(" = ");
-            this.tables[tableName].data = this.tables[tableName].data.filter(function (row) {
-                return row[columnWhere] !== valueWhere;
-            });
-        } else {
-            this.tables[tableName].data = [];
-        }
-    }
 
-    execute(statement) {
-        const result = this.parser.parse(statement);
-        if (result) {
-            return this[result.command](result.parsedStatement);
-        }
-        const message = `Syntax error: "${statement}"`;
-        throw new DatabaseError(statement, message);
-    }
-}
-try {
-    const database = new Database();
-    database.execute("create table author (id number, name string, age number, city string, state string, country string)");
-    database.execute("insert into author (id, name, age) values (1, Douglas Crockford, 62)");
-    database.execute("insert into author (id, name, age) values (2, Linus Torvalds, 47)");
-    database.execute("insert into author (id, name, age) values (3, Martin Fowler, 54)");
-    database.execute("delete from author where id = 2");
-    console.log(JSON.stringify(database.execute("select name, age from author"), undefined, "  "));
-} catch (e) {
-    console.log(e.message);
-}
+const languages = createArray();
+
+languages[0] = 'Python';
+languages[1] = 'JavaScript';
+languages[2] = 'Java';
+console.log(languages);
+
+delete languages[1];
+console.log(languages);
